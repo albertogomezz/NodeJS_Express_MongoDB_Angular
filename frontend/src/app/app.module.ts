9b53dd268a7c0e6600f261de2ef5f19f6bbb3bdc
Alberto <albertogom.alon@gmail.com>
2023-09-28 19:28:43 +0200

    List Category Update

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// import { CoreModule } from './core/core.module';
 import { FooterComponent, HeaderComponent } from './shared/layout/index';
+
+// Toastr
+// import { ToastrModule } from 'ngx-toastr';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -11,8 +18,11 @@ import { FooterComponent, HeaderComponent } from './shared/layout/index';
     HeaderComponent,
   ],
   imports: [
+    // CoreModule,
     BrowserModule,
+    // BrowserAnimationsModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/shared/list-categories/list-categories.component.ts
+++ frontend/src/app/shared/list-categories/list-categories.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../../core/services/cateogry.service'
+import { Category } from 'src/app/core/models/category.model';
 
 @Component({
   selector: 'app-list-categories',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class ListCategoriesComponent {
 
+  categories: Category[] = [];
+
+  constructor(private CategoryService: CategoryService) { }
+
+  //INICIA 
+
+  ngOnInit(): void {
+    this.getCategories();
+  }
+
+  // TOTES LES CATEGORIES
+  getCategories() {
+    // const params = this.getRequestParams(this.offset, 3);
+    this.CategoryService.all_categories().subscribe(
+      (data: any) => {
+        this.categories = data.categories;
+        console.log(this.categories);        
+      }
+    );
+  }
 }
