ba1adc6f2b24e461d46d3aaae3d6ff750a20433a
Kiko Gilabert <kiko.gilabertm@gmail.com>
2023-10-16 18:01:30 +0200

    dddd

--- frontend/src/app/shared/list-products/list-products.component.ts
+++ frontend/src/app/shared/list-products/list-products.component.ts
@@ -19,13 +19,6 @@ export class ListProductsComponent implements OnInit {
   routeFilters!: string | null;
   products: Product[] = [];
   slug_Category!: string | null;
-<<<<<<< HEAD
-  offset = 0;
-  limit = 10;
-  totalPages: Array<number> = [];
-  listCategories: Category[] = [];
-  filters!: Filters;
-=======
   routeFilters!: string | null;
 
   listCategories: Category[] = [];
@@ -34,15 +27,7 @@ export class ListProductsComponent implements OnInit {
   limit: number = 3;
   totalPages: Array<number> = [];
   currentPage: number = 1;
->>>>>>> 076a561d75f5577912813d0a8fcd6e4ed1230d1f
 
-
-  listCategories: Category[] = [];
-  filters = new Filters();
-  offset: number = 0;
-  limit: number = 3;
-  totalPages: Array<number> = [];
-  currentPage: number = 1;
 
   constructor(private productService: ProductService, private ActivatedRoute: ActivatedRoute, private CategoryService: CategoryService){}
 
@@ -50,40 +35,6 @@ export class ListProductsComponent implements OnInit {
   ngOnInit(): void {
     console.log()
     this.slug_Category = this.ActivatedRoute.snapshot.paramMap.get('slug');
-<<<<<<< HEAD
-    this.routeFilters = this.ActivatedRoute.snapshot.paramMap.get('filters');
-    this.get_products();
-    this.getListForCategory();
-  }
-  getRequestParams(offset: number, limit: number): any {
-    let params: any = {};
-
-    params[`offset`] = offset;
-    params[`limit`] = limit;
-
-    return params;
-  }
-  //Agarrar productes
-  get_products(): void {
-    const params = this.getRequestParams(this.offset, this.limit);
-    if (this.slug_Category !== null) { 
-      this.productService.getProductsByCategory(this.slug_Category, params).subscribe(
-        (data : any) => {
-          console.log(data);
-          if (this.slug_Category) {
-            this.filters.category = this.slug_Category;
-          }
-          this.products = data.products;
-          // this.totalPages = Array.from(new Array(Math.ceil(data.product_count/this.limit)), (val, index) => index + 1);
-          // console.log(this.products[0].images);
-        });
-    } else if (this.routeFilters !== null) {
-      this.refresRouteFilter();
-      this.get_list_filtered(this.filters);
-    } else {
-      this.get_list_filtered(this.filters);
-    }
-=======
 
 
     this.getListForCategory();    
@@ -116,7 +67,6 @@ export class ListProductsComponent implements OnInit {
           this.totalPages = Array.from(new Array(Math.ceil(data.product_count/this.limit)), (val, index) => index + 1);
           console.log(this.products);
       });
->>>>>>> 076a561d75f5577912813d0a8fcd6e4ed1230d1f
   }
 
   get_all_products() {
@@ -137,38 +87,6 @@ export class ListProductsComponent implements OnInit {
     );
   }
 
-<<<<<<< HEAD
-
-  //FER FUNCIO SENSE OFFSET I LIMIT
-  //FER FUNCIO SENSE OFFSET I LIMIT
-  //FER FUNCIO SENSE OFFSET I LIMIT
-  //FER FUNCIO SENSE OFFSET I LIMIT
-  //FER FUNCIO SENSE OFFSET I LIMIT
-  
-
-
-  get_list_filtered(filters: Filters) {
-    this.filters = filters;
-    // console.log(this.filters);
-    this.productService.get_products(this.filters).subscribe(
-      (data: any) => {
-        console.log(data);
-        // this.products = data.products;
-        // console.log(this.products);
-        // this.totalPages = Array.from(new Array(Math.ceil(data.product_count/this.limit)), (val, index) => index + 1);
-      }
-    );
-  }
-  
-
-  refresRouteFilter() {
-    this.routeFilters = this.ActivatedRoute.snapshot.paramMap.get('filters');
-    if(typeof(this.routeFilters) == "string" ){
-      this.filters = JSON.parse(atob(this.routeFilters));
-    }else{
-      this.filters = new Filters();
-    }
-=======
   setPageTo(pageNumber: number) {
 
     this.currentPage = pageNumber;
@@ -184,7 +102,6 @@ export class ListProductsComponent implements OnInit {
 
     this.Location.replaceState('/shop/' + btoa(JSON.stringify(this.filters)));
     this.get_list_filtered(this.filters);
->>>>>>> 076a561d75f5577912813d0a8fcd6e4ed1230d1f
   }
 }
 
